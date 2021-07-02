04bf6a6483218bfe36d4a44294eafa2f619b7725
tanqing <tanqing.cc@bytedance.com>
2021-07-02 10:37:34 +0800

    tab2功能实现

--- app/src/main/java/com/example/demo/Fragment2.kt
+++ app/src/main/java/com/example/demo/Fragment2.kt
@@ -1,12 +1,21 @@
 package com.example.demo
 
 import android.os.Bundle
+import android.provider.CallLog
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment2.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Fragment2 : Fragment() {
+
+    private val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,7 +24,18 @@ class Fragment2 : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
+        val root = "/storage/emulated/0/"
+        val files = File(root).listFiles()
+        if (!files.isNullOrEmpty()) {
+            var text = ""
+            for (f in files) {
+                if (f.isDirectory) {
+                    val time = f.lastModified()
+                    text += f.name + "  " + sdf.format(time) + "\n"
+                }
+            }
+            tv_show2.text = text
+        }
     }
 
 }
